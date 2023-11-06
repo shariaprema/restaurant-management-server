4dadbc796625fb99239f77626354574e07574cd3
shariaprema <shariaprema@gmail.com>
2023-11-06 13:25:40 +0600

    create fake json data for 6 top-selling Food items and insert in mongodb. And read the data in localhost

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.socuaah.mongodb.net/?retryWrites=true&w=majority`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,12 +27,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const topFoodsCollection = client.db("restaurantDB").collection("topFoods");
+
+
+
+    // TopFoodsCollection:
+    app.get('/topFoods', async(req,res)=>{
+        const cursor = topFoodsCollection.find();
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
